9ba437a4cb9b6d347a8d25a82d5d36bf0e0917cc
trungbq278 <trungbui1997@gmail.com>
2019-07-16 17:08:12 +0700

    abc

--- NhaTro/NhaTro/Controllers/HomeController.cs
+++ NhaTro/NhaTro/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NhaTro.Models;
 
 namespace NhaTro.Controllers
 {
+    
     public class HomeController : Controller
     {
+        QuanLyNhaTroDbContext db = new QuanLyNhaTroDbContext();
         // GET: Home
         public ActionResult Index()
         {
