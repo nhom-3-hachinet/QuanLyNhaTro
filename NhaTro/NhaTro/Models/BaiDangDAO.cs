dddcd2317c5081c420b8603715646fa3dcc46302
trungbq278 <trungbui1997@gmail.com>
2019-07-24 13:46:15 +0700

    COMMIT

--- NhaTro/NhaTro/Models/BaiDangDAO.cs
+++ NhaTro/NhaTro/Models/BaiDangDAO.cs
@@ -8,24 +8,12 @@ namespace NhaTro.Models
     
     public class BaiDangDAO
     {
-        QuanLyNhaTroDbContext db = new QuanLyNhaTroDbContext();
-        
-        public List<BaiDang> ListBaiDangChinh()
-        {
-            var model = (from a in db.BaiDangs
-                         join b in db.QuanHuyens on a.MaQH equals b.MaQH
-                         join c in db.TinhTPs on a.MaTTP equals c.MaTTP
-                         where a.Moi == 1
-                         select new BaiDang()
-                         {
-                             TieuDe = a.TieuDe,
-                             Gia = a.Gia,
-                             DiaChi = a.DiaChi,
-                             SDT = a.SDT,
-                             //TenQH = b.TenQH,
-                             //TenTTP = c.TenTTP
-                         });
-            return model.ToList();
-        }
+        public string TieuDe { get; set; }
+        public string Gia { get; set; }
+        public string AnhBia { get; set; }
+        public string DiaChi { get; set; }
+        public string TenQH { get; set; }
+        public string TenTTP { get; set; }
+        public string SDT { get; set; }
     }
 }
