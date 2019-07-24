090b139ac25196b4dc7cc71f3ffb1ea6d81253df
dzunglek56 <bomdung5@gmail.com>
2019-07-24 15:22:05 +0700

    Themadmin

---
+++ NhaTro/NhaTro/Controllers/QuanLySanPhamController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NhaTro.Models;
+
+namespace NhaTro.Controllers
+{
+    public class QuanLySanPhamController : Controller
+    {
+        // GET: QuanLySanPham
+        QuanLyNhaTroDbContext db = new QuanLyNhaTroDbContext();
+        public ActionResult Index()
+        {
+            return View(db.KhachHangs.ToList());
+        }
+    }
+}
